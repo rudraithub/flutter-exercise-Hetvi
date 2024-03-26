00880540da0c8ed6a3e0c7f88d780526de1cc89d
hetvi8099 <hetvi.patel8099@gmail.com>
2024-03-26 10:32:59 +0530

    add

---
+++ 06-operator/0602-arithmetic2.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 99;
+  int b = 12;
+  print(a * b);
+  print(a + b);
+  print(a - b);
+  print(a / b);
+  print(a % b);
+}
---
+++ 06-operator/0604assignmant.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 52;
+  int b = 45;
+  print(a += b);
+  print(a -= b);
+  print(a *= b);
+  print(a ~/= b);
+  print(a %= b);
+}
