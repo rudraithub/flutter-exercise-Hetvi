824df18e93c62305cb99673da0af5fe11d646e5f
hetvi8099 <hetvi.patel8099@gmail.com>
2024-03-27 12:48:44 +0530

    add

---
+++ 09-switchcase.dart/0901-positive.dart
@@ -0,0 +1,18 @@
+void main() {
+  int a = 23;
+  switch (a < 20) {
+    case 1:
+      print("23 is negative");
+
+      break;
+    case 2:
+      print("23 is positive");
+      break;
+    case 3:
+      print("23 is zero");
+      break;
+
+    default:
+      print("out of range");
+  }
+}
