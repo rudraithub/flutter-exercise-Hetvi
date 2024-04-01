caf36b3db83cf33b24cf52eee13012943fe2afd5
hetvi8099 <hetvi.patel8099@gmail.com>
2024-04-01 10:49:17 +0530

    add

---
+++ 11-classobject.dart/111-mahipatel.dart
@@ -0,0 +1,32 @@
+class rollnumber {
+  int a = 01;
+}
+
+class name {
+  String a = "mahi patel";
+}
+
+class dateofbirth {
+  int a = 01 - 01 - 1011;
+}
+
+class mobile {
+  int a = 9999999999;
+}
+
+class city {
+  String a = "bhavnagar";
+}
+
+void main() {
+  var object = rollnumber();
+  var object1 = name();
+  var object2 = dateofbirth();
+  var object3 = mobile();
+  var object4 = city();
+  print(object.a);
+  print(object1.a);
+  print(object2.a);
+  print(object3.a);
+  print(object4.a);
+}
