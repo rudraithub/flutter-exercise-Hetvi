c73420fc9853acc8f164ad1d5b75f3f74f148cb1
hetvi8099 <hetvi.patel8099@gmail.com>
2024-05-10 18:23:42 +0530

    add

---
+++ 1801-hetvi.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "hetvi patel";
+  String dob = "nov 27, 1999";
+  String mobile = "9999999999";
+
+  print("Name   : $name");
+  print("DOB    : $dob");
+  print("Mobile : $mobile");
+}
